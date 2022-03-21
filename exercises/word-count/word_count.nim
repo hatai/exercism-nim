2fe5f4e64178ee7e5da333c1907b231f11962ad7
hatai <20625256+hatai@users.noreply.github.com>
2022-03-21 19:39:41 +0900

    refactor: update Word Count

---
+++ exercises/word-count/word_count.nim
@@ -0,0 +1,7 @@
+import tables, strutils, sequtils
+
+proc countWords*(str: string): CountTableRef[string] =
+    result = newCountTable[string]()
+    var parsed = str.toLower.split(AllChars - Letters - Digits - {'\''}).filterIt(it.len > 0)
+    for item in parsed:
+        result.inc(item.strip(chars = {'\''}))
--- exercises/word_count.nim
+++
@@ -1,8 +0,0 @@
-import tables, strutils, sequtils
-
-proc countWords*(str: string): CountTableRef[string] =
-    result = newCountTable[string]()
-    var parsed = str.toLower.split(AllChars - Letters - Digits - {'\''}).filterIt(it.len > 0)
-    for item in parsed:
-        result.inc(item.strip(chars = {'\''}))
-        
