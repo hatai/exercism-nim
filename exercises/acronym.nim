892681f67270ed489aac2c3b615ecd0da63332cb
hatai <20625256+hatai@users.noreply.github.com>
2022-03-21 19:34:06 +0900

    refactor: update Acronym

--- exercises/acronym.nim
+++
@@ -1,8 +0,0 @@
-import strutils, sequtils
-
-proc abbreviate*(input: string): string =
-    input.split({' ', '-', '_'})
-        .filterIt(it.len != 0)
-        .mapIt(it[0].toUpperAscii)
-        .join
-        
---
+++ exercises/acronym/acronym.nim
@@ -0,0 +1,7 @@
+import strutils, sequtils
+
+proc abbreviate*(input: string): string =
+    input.split({' ', '-', '_'})
+        .filterIt(it.len != 0)
+        .mapIt(it[0].toUpperAscii)
+        .join
