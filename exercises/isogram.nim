7f457018131cb560ad98679981b06913c39e52dd
hatai <20625256+hatai@users.noreply.github.com>
2022-03-21 19:36:11 +0900

    refactor: update Isogram

--- exercises/isogram.nim
+++
@@ -1,9 +0,0 @@
-import strutils, sequtils
-
-const
-    alphabets = {'a'..'z'}
-
-proc isIsogram*(input : string) : bool =
-    let filtered = input.map(toLowerAscii).filter(isLowerAscii)
-    return filtered.len == filtered.deduplicate().len
-    
---
+++ exercises/isogram/isogram.nim
@@ -0,0 +1,8 @@
+import strutils, sequtils
+
+const
+    alphabets = {'a'..'z'}
+
+proc isIsogram*(input : string) : bool =
+    let filtered = input.map(toLowerAscii).filter(isLowerAscii)
+    return filtered.len == filtered.deduplicate().len
