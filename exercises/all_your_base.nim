e5e97b604fd17c1fb4d0616718f745daeeae0d95
hatai <20625256+hatai@users.noreply.github.com>
2022-03-21 17:15:29 +0900

    feat: All Your Base

---
+++ exercises/all_your_base.nim
@@ -0,0 +1,26 @@
+import algorithm, math
+
+proc convert*(inputDigits: openarray[int], inputBase, outputBase: int): seq[int] =
+    if inputBase < 2 or outputBase < 2:
+        raise newException(ValueError, "inputBase and putputBase must be bigger than 2.")
+
+    let reversedInputDigits = reversed(inputDigits)
+    var outputDigits: seq[int]
+    var base10 = 0
+
+    for i in 0..<inputDigits.len:
+        let digit = reversedInputDigits[i]
+
+        if digit < 0 or digit >= inputBase:
+            raise newException(ValueError, "element of inputDigits must be '0 <= e < inputBase'")
+
+        base10 += digit*inputBase^i
+
+    while base10 != 0:
+        outputDigits.add(base10.mod(outputBase))
+        base10 = (base10 / outputBase).floor.toInt
+
+    if outputDigits.len == 0:
+        outputDigits.add(0)
+
+    return outputDigits.reversed
