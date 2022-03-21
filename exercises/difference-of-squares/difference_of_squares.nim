61e12bda1f8feb26b440f45a56c82245e5fc2d78
hatai <20625256+hatai@users.noreply.github.com>
2022-03-21 19:35:14 +0900

    refactor: update Difference Of Aquares

---
+++ exercises/difference-of-squares/difference_of_squares.nim
@@ -0,0 +1,10 @@
+import math, sequtils
+
+proc squareOfSum*(x: int): int =
+    toSeq(1..x).sum^2
+
+proc sumOfSquares*(x: int): int =
+    toSeq(1..x).mapIt(it^2).sum
+
+proc difference*(x: int): int =
+    squareOfSum(x) - sumOfSquares(x)
