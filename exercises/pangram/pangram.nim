5bfd568c0c04a773f580d00c96b5dc96fc0ae8b0
hatai <20625256+hatai@users.noreply.github.com>
2022-03-21 19:37:14 +0900

    refactor: update Pangram

---
+++ exercises/pangram/pangram.nim
@@ -0,0 +1,15 @@
+import strutils, sequtils
+
+type
+    CharSet = set[char]
+
+const
+    alphabetsLen = {'a'..'z'}.card
+
+proc isPangram*(value: string): bool =
+    return value
+        .map(toLowerAscii)
+        .filter(isLowerAscii)
+        .deduplicate()
+        .len == alphabetsLen
+        
