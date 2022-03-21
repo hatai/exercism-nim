0b4dc13a132be4171322b05f605adf707ebddc7f
hatai <20625256+hatai@users.noreply.github.com>
2022-03-21 17:56:02 +0900

    feat: Darts

---
+++ exercises/darts.nim
@@ -0,0 +1,17 @@
+import math
+
+type
+    Coord = tuple[x: float, y: float]
+
+proc score*(c: Coord) : int =
+    var distance = float64(c.x^2 + c.y^2).sqrt
+
+    if distance <= 1:
+        10
+    elif distance > 1 and distance <= 5:
+        5
+    elif distance > 5 and distance <= 10:
+        1
+    else:
+        0
+        
