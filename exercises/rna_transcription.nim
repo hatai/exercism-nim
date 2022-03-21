ae5f2f9eb439977c094910d48936897e7551b980
hatai <20625256+hatai@users.noreply.github.com>
2022-03-21 18:04:28 +0900

    feat: Rna Transcription

---
+++ exercises/rna_transcription.nim
@@ -0,0 +1,12 @@
+import tables, sequtils
+
+const
+    t = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}.toTable
+
+proc toRna*(input : string) : string =
+    var output: string
+    for c in input:
+        output.add(t[c])
+
+    return output
+    
