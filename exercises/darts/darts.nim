12b49804f166ac2b3314dacdc4837d4e8d37b60b
hatai <20625256+hatai@users.noreply.github.com>
2022-03-21 19:34:50 +0900

    refactor: update Darts

--- exercises/darts/darts.nim
+++ exercises/darts/darts.nim
@@ -5,7 +5,6 @@ type
 
 proc score*(c: Coord) : int =
     var distance = float64(c.x^2 + c.y^2).sqrt
-
     if distance <= 1:
         10
     elif distance > 1 and distance <= 5:
@@ -14,4 +13,3 @@ proc score*(c: Coord) : int =
         1
     else:
         0
-        
