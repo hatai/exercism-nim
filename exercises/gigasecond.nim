cbd6196e288c723c8856c6872820f8ebd4a660d7
hatai <20625256+hatai@users.noreply.github.com>
2022-03-21 17:57:03 +0900

    feat: Gigasecond

---
+++ exercises/gigasecond.nim
@@ -0,0 +1,9 @@
+import times
+
+const
+    gigaseconds = int64(1e9)
+    dur = initDuration(seconds = gigaseconds)
+
+proc addGigasecond*(moment : DateTime) : DateTime = 
+    return moment + dur
+    
